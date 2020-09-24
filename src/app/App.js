eb65a4271b2c54fda8c45e9a63e0cbbd5516131c
ritwikvd <ritwikvd@gmail.com>
2020-09-24 11:59:13 +0530

    removing unused imports

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route as R, Switch } from "react-router-dom";
 import useErrorBoundary from "use-error-boundary";
 import "../styles/main.scss";
